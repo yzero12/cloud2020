fb3d734a5e3cdb1420af7f7565eb6997ffa7c02c
yzero12 <2358702934@qq.com>
2023-02-22 11:42:03 +0800

    sencond commit

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,5 +1,9 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.atguigu.springcloud.alibaba.myhandler.CustomerHandler;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +22,11 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "------testB";
+    }
+
+    @GetMapping(value = "/customerHandler")
+    @SentinelResource(value = "customerHandler", blockHandlerClass = CustomerHandler.class, blockHandler = "customerHandler1")
+    public CommonResult customerHandler() {
+        return new CommonResult(200, "success", null);
     }
 }
--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/myhandler/CustomerHandler.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/myhandler/CustomerHandler.java
@@ -1,13 +1,18 @@
 package com.atguigu.springcloud.alibaba.myhandler;
 
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entities.CommonResult;
+import org.springframework.stereotype.Component;
+
 /**
  * @author zjzstart
  * @create 2023-02-22 10:14
  */
+@Component
 public class CustomerHandler {
 
-    public static String customerHandler(){
-        return "global hander";
+    public static CommonResult customerHandler1(BlockException blockException){
+        return new CommonResult(444,"自定义block",null);
     }
 
 }
