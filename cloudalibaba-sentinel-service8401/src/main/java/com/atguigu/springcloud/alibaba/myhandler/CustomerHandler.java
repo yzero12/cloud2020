c21c6f7a1c7430d41fb5b7db5b3eff32498c09f0
yzero12 <2358702934@qq.com>
2023-02-22 10:25:37 +0800

    first commit

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/myhandler/CustomerHandler.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.alibaba.myhandler;
+
+/**
+ * @author zjzstart
+ * @create 2023-02-22 10:14
+ */
+public class CustomerHandler {
+
+    public static String customerHandler(){
+        return "global hander";
+    }
+
+}
